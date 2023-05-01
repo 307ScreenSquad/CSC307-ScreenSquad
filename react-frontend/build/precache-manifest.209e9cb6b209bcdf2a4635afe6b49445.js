10f091e817b07f75cfd573f32fe60f211278366e
Nicholas Aspiras <57922966+NickAspiras@users.noreply.github.com>
2023-05-01 11:47:03 -0700

    Add files via upload

---
+++ react-frontend/build/precache-manifest.209e9cb6b209bcdf2a4635afe6b49445.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "5dca97e52d1dcb52abed",
+    "url": "/static/css/main.5acbbc88.chunk.css"
+  },
+  {
+    "revision": "5dca97e52d1dcb52abed",
+    "url": "/static/js/main.5dca97e5.chunk.js"
+  },
+  {
+    "revision": "e36bd727ad85ecf6d82e",
+    "url": "/static/js/1.e36bd727.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "2eb467b347cc7e4cf8578176d868d787",
+    "url": "/index.html"
+  }
+];
--- react-frontend/src/App.js
+++ react-frontend/src/App.js
@@ -1,23 +1,69 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {useState, useEffect} from 'react'
+import Table from './Table'
+import Form from './Form'
+import axios from 'axios'
 
 function App() {
+  const [characters, setCharacters] = useState([]);
+
+  function removeOneCharacter (index) {
+    const updated = characters.filter(async (character, i) => {
+        if(i === index){
+          console.log(character);
+          await axios.delete('http://localhost:8000/users/' + character._id);
+        }
+        return i !== index
+    });
+    setCharacters(updated);
+  }
+  
+
+  function updateList(person) {
+    makePostCall(person).then(result => {
+      if(result && result.status === 201){
+        setCharacters([...characters, person]);
+      }
+    })
+    
+  }
+
+  async function fetchAll(){
+    try {
+       const response = await axios.get('http://localhost:8000/users');
+       return response.data.users_list;     
+    }
+    catch (error){
+       //We're not handling errors. Just logging into the console.
+       console.log(error); 
+       return false;         
+    }
+  }
+  useEffect(() => {
+      fetchAll().then( result => {
+          if(result){
+              //console.log(result);
+              setCharacters(result);
+          }
+      })
+  })
+
+  async function makePostCall(person){
+    try {
+       const response = await axios.post('http://localhost:8000/users', person);
+       return response;     
+    }
+    catch (error){
+       //We're not handling errors. Just logging into the console.
+       console.log(error); 
+       return false;         
+    }
+  }
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <Table characterData={characters} removeCharacter={removeOneCharacter}/>
+      <Form handleSubmit={updateList}/>
     </div>
   );
 }

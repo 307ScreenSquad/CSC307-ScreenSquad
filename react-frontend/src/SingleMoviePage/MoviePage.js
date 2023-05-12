467b8b00cdcda3b6f677d9cd17824d61200ae5e6
nickambrose7 <nickambrose7@gmail.com>
2023-05-12 11:02:04 -0700

    Done

--- react-frontend/src/SingleMoviePage/MoviePage.js
+++ react-frontend/src/SingleMoviePage/MoviePage.js
@@ -1,47 +1,54 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import './MoviePage.css';
+import { useParams } from 'react-router-dom';
+// I'm going to go through this file, I want you to tell me what the code is doing
 
-const MoviePage = ({ movieId }) => {
+const MoviePage = () => { 
+  const { movieId } = useParams(); 
   const [movie, setMovie] = useState(null);
+  const [cast, setCast] = useState([]);
+  const [streamingPlatforms, setStreamingPlatforms] = useState([]);
 
   useEffect(() => {
-    async function fetchMovie() {
+    async function fetchMovieDetails() {
       try {
-        const response = await axios.get(`YOUR_BACKEND_URL/movies/${movieId}`);
-        setMovie(response.data);
+        const movieResponse = axios.get(`https://api.themoviedb.org/3/movie/${movieId}?api_key=a43aea022f03ee960884520d48d1c5f8`);
+        const castResponse = axios.get(`https://api.themoviedb.org/3/movie/${movieId}/credits?api_key=a43aea022f03ee960884520d48d1c5f8`);
+        const streamingPlatformsResponse = axios.get(`https://api.themoviedb.org/3/movie/${movieId}/watch/providers?api_key=a43aea022f03ee960884520d48d1c5f8`);
+
+        const responses = await Promise.all([movieResponse, castResponse, streamingPlatformsResponse]); 
+
+        setMovie(responses[0].data);
+        setCast(responses[1].data.cast.map(castMember => castMember.name));
+        
+        setStreamingPlatforms(Object.values(responses[2].data.results.US.flatrate || {}).map(provider => provider.provider_name)); 
+        
+
       } catch (error) {
-        console.error('Error fetching movie:', error);
+        console.error('Error fetching movie details:', error);
       }
     }
 
-    fetchMovie();
+    fetchMovieDetails();
   }, [movieId]);
 
-  // Uncomment the below two lines of code once the backend is set up.
-  //if (!movie) return <div>Loading...</div>;
+  if (!movie || !cast || !streamingPlatforms) return <div>Loading...</div>;
 
-  // Extract required fields
-//   const { title, rating, genres, runtime, synopsis, cast, poster, streamingPlatforms } = movie;
-  //set the variables with test values since the backend is not working yet
-    const title = "test title";
-    const rating = "test rating";
-    const genres = ["test genre 1", "test genre 2"];
-    const runtime = "test runtime";
-    const synopsis = "test synopsis";
-    const cast = ["test cast 1", "test cast 2"];
-    const poster = "test poster";
-    const streamingPlatforms = ["test streaming platform 1", "test streaming platform 2"];
-    
+  const { title, vote_average, genres, runtime, overview, poster_path } = movie;
 
-  // Render movie details
+  const rating = vote_average;
+  const synopsis = overview;
+  const poster = `https://image.tmdb.org/t/p/w500${poster_path}`;
+  const genresList = genres.map(genre => genre.name).join(', ');
+
   return (
     <div className="movie-page">
       <img src={poster} alt={title} className="movie-page__poster" />
       <div className="movie-page__details">
         <h1>{title}</h1>
         <p>Rating: {rating}</p>
-        <p>Genres: {genres.join(', ')}</p>
+        <p>Genres: {genresList}</p>
         <p>Runtime: {runtime} minutes</p>
         <p>Synopsis: {synopsis}</p>
         <p>Cast: {cast.join(', ')}</p>

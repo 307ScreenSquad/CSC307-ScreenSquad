91df2fae30b5f6e76293a4d6338badc97c621074
nickambrose7 <nickambrose7@gmail.com>
2023-05-23 10:22:55 -0700

    took away useless stuff

--- react-frontend/src/App.js
+++ react-frontend/src/App.js
@@ -81,38 +81,6 @@ function App() {
     }
   }
 
-  // function removeOneCharacter (index) {
-  //   const updated = characters.filter(async (character, i) => {
-  //       if(i === index){
-  //         console.log(character);
-  //         await axios.delete('http://localhost:8000/users/' + character._id);
-  //       }
-  //       return i !== index
-  //   });
-  //   setCharacters(updated);
-  // }
-  
-
-  // function updateList(person) {
-  //   makePostCall(person).then(result => {
-  //     if(result && result.status === 201){
-  //       setCharacters([...characters, person]);
-  //     }
-  //   })
-    
-  // }
-
-  // async function fetchAll(){
-  //   try {
-  //      const response = await axios.get('http://localhost:8000/users');
-  //      return response.data.users_list;
-  //   }
-  //   catch (error){
-  //      //We're not handling errors. Just logging into the console.
-  //      console.log(error); 
-  //      return false;         
-  //   }
-  // }
 
   useEffect(() => {
     const existingName = localStorage.getItem('name');
@@ -121,27 +89,7 @@ function App() {
       setLoggedIn(true);
     }
   }, [])
-
-  // async function makePostCall(person){
-  //   try {
-  //      const response = await axios.post('http://localhost:8000/users', person);
-  //      return response;     
-  //   }
-  //   catch (error){
-  //      //We're not handling errors. Just logging into the console.
-  //      console.log(error); 
-  //      return false;         
-  //   }
-  // }
-
-  /*
-  return (
-    <div className="container">
-      <Table characterData={characters} removeCharacter={removeOneCharacter}/>
-      <Form handleSubmit={updateList}/>
-    </div>
-  );
-  */
+  
 
   return(
     <ThemeProvider theme={theme}>

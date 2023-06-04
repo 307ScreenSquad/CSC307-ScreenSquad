3fc4f3552f10137fd8e3f28c21306029eb1bcfd1
nickambrose7 <nickambrose7@gmail.com>
2023-06-04 14:43:02 -0700

    Got page up and running

--- react-frontend/src/App.js
+++ react-frontend/src/App.js
@@ -1,27 +1,25 @@
-import React, { useState, useEffect } from 'react';
-import MoviePage from './SingleMoviePage/MoviePage';
-// import MovieSearch from './SingleMoviePage/MovieSearch';
-import './App.css';
-import axios from 'axios'
-import { BrowserRouter, Routes, Route } from "react-router-dom"
-import Login from "./Login"
-import Register from "./Register"
-import Forgot from "./Forgot"
-import theme from "./theme"
-import Base from "./base"
-import Landing from "./Landing"
-import NavBar from "./NavBar"
-import Admin from "./Admin"
-import Profile from "./Profile"
-import MyWatchlist from "./WatchList/MyWatchlist"
-import bcrypt from 'bcryptjs'
-import { ThemeProvider } from "@emotion/react"; 
-
-
+import React, { useState, useEffect } from "react";
+import MoviePage from "./SingleMoviePage/MoviePage";
+import "./App.css";
+import axios from "axios";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Login from "./Login";
+import Register from "./Register";
+import Forgot from "./Forgot";
+import theme from "./theme";
+import Base from "./base";
+import Landing from "./Landing";
+import NavBar from "./NavBar";
+import Admin from "./Admin";
+import Profile from "./Profile";
+import MyWatchlist from "./WatchList/MyWatchlist";
+import UpcomingPage from "./UpcomingReleasesPage/UpcomingReleases";
+import bcrypt from "bcryptjs";
+import { ThemeProvider } from "@emotion/react";
 
 // hashing: https://medium.com/boca-code/how-to-encrypt-password-in-your-react-app-before-you-send-it-to-the-api-6e10a06f0a8e
 // SALT should be created ONE TIME upon sign up
-const salt = bcrypt.genSaltSync(10)
+const salt = bcrypt.genSaltSync(10);
 
 function App() {
   // const [characters, setCharacters] = useState([]);
@@ -31,89 +29,93 @@ function App() {
   //const passwordInputRef = useRef()
 
   async function handleCreateUser(name, email, password) {
-    const hashedPassword = bcrypt.hashSync(password, salt) // hash created previously created upon sign up
+    const hashedPassword = bcrypt.hashSync(password, salt); // hash created previously created upon sign up
     try {
-      const response = await axios.post('http://localhost:8000/register', {name: name, email: email, password: hashedPassword});
-      if(response.status === 201){
-        const responseUser = await axios.get(`http://localhost:8000/users/${email}`);
-        localStorage.setItem('name', responseUser.data.name);
-        localStorage.setItem('email', responseUser.data.email);
-        localStorage.setItem('isAdmin', responseUser.data.isAdmin);
-        localStorage.setItem('id', responseUser.data._id);
+      const response = await axios.post("http://localhost:8000/register", {
+        name: name,
+        email: email,
+        password: hashedPassword,
+      });
+      if (response.status === 201) {
+        const responseUser = await axios.get(
+          `http://localhost:8000/users/${email}`
+        );
+        localStorage.setItem("name", responseUser.data.name);
+        localStorage.setItem("email", responseUser.data.email);
+        localStorage.setItem("isAdmin", responseUser.data.isAdmin);
+        localStorage.setItem("id", responseUser.data._id);
         setLoggedIn(true);
       }
-      return response;     
-    }
-    catch (error){
-        //We're not handling errors. Just logging into the console.
-        console.log(error); 
-        return false;         
+      return response;
+    } catch (error) {
+      //We're not handling errors. Just logging into the console.
+      console.log(error);
+      return false;
     }
   }
 
   async function handleSubmitUser(email, password) {
     //const hashedPassword = bcrypt.hashSync(password, salt) // hash created previously created upon sign up
     try {
-      const response = await axios.post('http://localhost:8000/login', {email: email, password: password});
-      if(response){
-        const findUserResponse = await axios.get('http://localhost:8000/users/' + email);
+      const response = await axios.post("http://localhost:8000/login", {
+        email: email,
+        password: password,
+      });
+      if (response) {
+        const findUserResponse = await axios.get(
+          "http://localhost:8000/users/" + email
+        );
         console.log("find user", findUserResponse);
-        
-        localStorage.setItem('name', findUserResponse.data.name);
-        localStorage.setItem('email', findUserResponse.data.email);
-        localStorage.setItem('isAdmin', findUserResponse.data.isAdmin);
-        localStorage.setItem('id', findUserResponse.data._id);
+
+        localStorage.setItem("name", findUserResponse.data.name);
+        localStorage.setItem("email", findUserResponse.data.email);
+        localStorage.setItem("isAdmin", findUserResponse.data.isAdmin);
+        localStorage.setItem("id", findUserResponse.data._id);
         setLoggedIn(true);
-        
       }
-      return response;     
-    }
-    catch (error){
-        //We're not handling errors. Just logging into the console.
-        console.log(error); 
-        return false;         
+      return response;
+    } catch (error) {
+      //We're not handling errors. Just logging into the console.
+      console.log(error);
+      return false;
     }
   }
-  async function handleLogoutUser(){
+  async function handleLogoutUser() {
     try {
-      const response = await axios.post('http://localhost:8000/logout');
-      if(response){
-        localStorage.setItem('name', '');
-        localStorage.setItem('email', '');
-        localStorage.setItem('isAdmin', false);
-        localStorage.setItem('id', 0);
-        
-        
+      const response = await axios.post("http://localhost:8000/logout");
+      if (response) {
+        localStorage.setItem("name", "");
+        localStorage.setItem("email", "");
+        localStorage.setItem("isAdmin", false);
+        localStorage.setItem("id", 0);
+
         setLoggedIn(false);
-        
       }
-      return response;     
-    }
-    catch (error){
-        //We're not handling errors. Just logging into the console.
-        console.log(error); 
-        return false;         
+      return response;
+    } catch (error) {
+      //We're not handling errors. Just logging into the console.
+      console.log(error);
+      return false;
     }
   }
-  
+
   useEffect(() => {
-    const existingName = localStorage.getItem('name');
-    if(existingName){
+    const existingName = localStorage.getItem("name");
+    if (existingName) {
       console.log("Welcome " + existingName);
       setLoggedIn(true);
     }
-  })
-
+  }, []);
 
   // async function makePostCall(person){
   //   try {
   //      const response = await axios.post('http://localhost:8000/users', person);
-  //      return response;     
+  //      return response;
   //   }
   //   catch (error){
   //      //We're not handling errors. Just logging into the console.
-  //      console.log(error); 
-  //      return false;         
+  //      console.log(error);
+  //      return false;
   //   }
   // }
 
@@ -126,21 +128,27 @@ function App() {
   );
   */
 
-
-  return(
+  return (
     <ThemeProvider theme={theme}>
       <BrowserRouter>
-      <NavBar isLoggedIn={loggedIn} logoutUser={handleLogoutUser}/>
+        <NavBar isLoggedIn={loggedIn} logoutUser={handleLogoutUser} />
         <Routes>
-          <Route path="/" element={<Base />}/>
-          <Route path="/login" element={<Login submitUser={handleSubmitUser}/>} />
-          <Route path="/register" element={<Register createUser={handleCreateUser}/>} />
+          <Route path="/" element={<Base />} />
+          <Route
+            path="/login"
+            element={<Login submitUser={handleSubmitUser} />}
+          />
+          <Route
+            path="/register"
+            element={<Register createUser={handleCreateUser} />}
+          />
           <Route path="/forgot" element={<Forgot />} />
           <Route path="/landing" element={<Landing />} />
           <Route path="/movie/:movieId" element={<MoviePage />} />
+          <Route path="/upcoming" element={<UpcomingPage />} />
           <Route path="/watchlist" element={<MyWatchlist />} />
           <Route path="/admin" element={<Admin />} />
-          <Route path="/profile" element={<Profile isLoggedIn={loggedIn}/>} />
+          <Route path="/profile" element={<Profile isLoggedIn={loggedIn} />} />
         </Routes>
       </BrowserRouter>
     </ThemeProvider>
---
+++ react-frontend/src/UpcomingReleasesPage/UpcomingReleases.js
@@ -0,0 +1,52 @@
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import { Card, Container, Row, Col, Image } from "react-bootstrap";
+import "./UpcomingReleases.css";
+
+const UpcomingReleases = () => {
+  const [upcomingMovies, setUpcomingMovies] = useState([]);
+
+  useEffect(() => {
+    async function fetchUpcomingMovies() {
+      try {
+        const response = await axios.get(
+          `https://api.themoviedb.org/3/movie/upcoming?api_key=a43aea022f03ee960884520d48d1c5f8`
+        );
+        setUpcomingMovies(response.data.results);
+      } catch (error) {
+        console.error("Error fetching upcoming movies:", error);
+      }
+    }
+
+    fetchUpcomingMovies();
+  }, []);
+
+  if (!upcomingMovies) return <div>Loading...</div>;
+
+  return (
+    <Container fluid className="upcoming-releases">
+      <Row>
+        {upcomingMovies.map((movie) => (
+          <Col md={3} key={movie.id}>
+            <Card className="mb-4">
+              <Image
+                src={`https://image.tmdb.org/t/p/w500${movie.poster_path}`}
+                alt={movie.title}
+                fluid
+              />
+              <Card.Body>
+                <Card.Title>{movie.title}</Card.Title>
+                <Card.Text>
+                  <strong>Release Date:</strong> {movie.release_date} <br />
+                  <strong>Description:</strong> {movie.overview}
+                </Card.Text>
+              </Card.Body>
+            </Card>
+          </Col>
+        ))}
+      </Row>
+    </Container>
+  );
+};
+
+export default UpcomingReleases;

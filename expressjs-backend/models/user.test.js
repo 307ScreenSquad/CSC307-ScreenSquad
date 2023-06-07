cdee64de8669f9b6e549f2d5def08b448bdf993d
hopieyimmie <47905779+hopieyimmie@users.noreply.github.com>
2023-06-07 12:53:27 -0700

    fixed a test case issue that fails CD

--- expressjs-backend/models/user.test.js
+++ expressjs-backend/models/user.test.js
@@ -189,63 +189,6 @@ test('user services get all users', async () => {
 
   const validationError = await user_services.getAllUsers();
   expect(validationError.length).toBeGreaterThan(1);
-  
 
 }
-);const Movie_User = require('./user.js');
-const mongoose = require('mongoose');
-
-test('Movie_User model works as expected', () => {
-    const movie_user = new Movie_User({
-        name: "Test User",
-        email: "johns@jon.com",
-        password: "password",
-        isAdmin: false,
-    });
-    expect(movie_user.name).toBe("Test User");
-    expect(movie_user.email).toBe("johns@jon.com");
-    expect(movie_user.password).toBe("password");
-    expect(movie_user.isAdmin).toBe(false);
-});
-
-test('user model catches password too short', () => {
-    const movie_user = new Movie_User({
-      name: 'Test User',
-      email: 'john@john.com',
-      password: 'pass',
-      isAdmin: false,
-    });
-    
-    const validationError = movie_user.validateSync();
-  
-    expect(validationError.errors.password).toBeTruthy();
-  });
-  
-test('user model catches invalid email (no @)', () => {
-    const movie_user = new Movie_User({
-      name: 'Test User',
-      email: 'johnjohn.com',
-      password: 'password',
-      isAdmin: false,
-    });
-    
-    const validationError = movie_user.validateSync();
-  
-    expect(validationError.errors.email).toBeTruthy();
-  }
-);
-
-test('user model catches invalid email (no .com)', () => {
-    const movie_user = new Movie_User({
-      name: 'Test User',
-      email: 'john@john',
-      password: 'password',
-      isAdmin: false,
-    });
-    
-    const validationError = movie_user.validateSync();
-  
-
-    expect(validationError.errors.email).toBeTruthy();
-  }
 );

b2067cc1532308df8b5faa4900de3617b763968d
hopieyimmie <47905779+hopieyimmie@users.noreply.github.com>
2023-06-07 12:42:44 -0700

    just deleted a line from index.js we don't need

--- expressjs-backend/index.js
+++ expressjs-backend/index.js
@@ -5,7 +5,6 @@ const nodemailer = require("nodemailer");
 const app = express();
 const port = 8000;
 const cors = require("cors");
-const userServices = require("./models/user-services");
 const userServices = require('./controllers/user-services');
 const movieReviewServices = require('./controllers/movie-review-services');
 const watchlistServices = require('./controllers/watchlist-services');
